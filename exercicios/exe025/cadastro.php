f4ffaf6447ccf2ae8b8cc7381279a25afbfd3794
Yoko Lucila Takano <148287663+yoko-takano@users.noreply.github.com>
2023-10-27 13:38:14 -0400

    exercicios sobre formulários

---
+++ exercicios/exe025/cadastro.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>O cadastro foi finalizado com sucesso.</p>
+    <p>Aprenda PHP para prosseguir com a análise.</p>
+</body>
+</html>
